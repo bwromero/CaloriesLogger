cf38da25097fcf07533e1fa9bac852f8003608c1
Bryan <bryan.romero@gft.com>
2023-01-08 02:02:03 +0100

    Edi recipe component form

--- src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -3,13 +3,16 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-recipe-edit',
   templateUrl: './recipe-edit.component.html',
-  styleUrls: ['./recipe-edit.component.css']
+  styleUrls: ['./recipe-edit.component.css'],
 })
 export class RecipeEditComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  preview(event) {
+    let frame = document.getElementById('frame') as HTMLImageElement;
+
+    frame.src = URL.createObjectURL(event.target.files[0]);
   }
-
 }
